fd867a338dbf3b4c3ce1b8fae3e812bc779d4413
Vimal Munjani <vimal.j.munjani@gmail.com>
2018-05-14 13:52:35 +0530

    implemented google login and logout

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { audit } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) {
+    afAuth.authState.subscribe(user => console.log(user));
+   }
 
-  ngOnInit() {
-  }
-
+   logout() {
+     this.afAuth.auth.signOut();
+   }
 }
