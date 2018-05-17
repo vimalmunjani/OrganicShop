7908d2b5f55abc5a9f94baf509807815b2048827
Vimal Munjani <vimal.j.munjani@gmail.com>
2018-05-17 12:13:37 +0530

    added filter to search products

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../product.service';
+import { AppProduct } from '../../models/app-product';
 
 @Component({
   selector: 'app-admin-products',
@@ -8,12 +9,26 @@ import { ProductService } from '../../product.service';
 })
 export class AdminProductsComponent implements OnInit {
 
-  products$;
-
+  products: AppProduct[];
+  filteredProducts: AppProduct[];
   constructor(private productService: ProductService) {
-    this.products$ = productService.getAllProducts();
+   productService.getAllProducts().subscribe(p => {
+     this.products = this.filteredProducts = p;
+   });
+   console.log(this.products);
    }
 
+
+   filterSearch(query: string) {
+
+    console.log(query);
+    this.filteredProducts = (query) ?
+     this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
+     this.products;
+
+     console.log(this.filteredProducts);
+
+   }
   ngOnInit() {
   }
 
